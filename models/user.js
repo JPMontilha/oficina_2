efd852ab326d94d5b63feb72de047e4913ee6e47
JPMontilha <jpvacilotomontilha@gmail.com>
2025-05-11 14:03:35 -0300

    feat: adicionando criptografia de senha

--- models/user.js
+++ models/user.js
@@ -1,8 +1,22 @@
 const mongoose = require('../db');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   senha: { type: String, required: true }
 }, { _id: false });
+
+//Criptografar a senha
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('senha')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.senha = await bcrypt.hash(this.senha, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = userSchema;
